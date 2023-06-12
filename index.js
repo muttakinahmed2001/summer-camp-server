af2d5a802d718520261736c1511e502eb67f6a3f
unknown <muttakinahmed871@gmail.com>
2023-06-12 22:51:58 +0600

    make api for verrify instructor

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req,res,next) => {
+const authorization = req.headers.authorization;
+if(!authorization){
+  return res.status(401).send({error:true, message: 'unauthorized access'});
+
+}
+const token = authorization.split(' ')[1];
+jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err, decoded) => {
+  if(err){
+    return res.status(401).send({error:true, message: 'unauthorized access'})
+  }
+  req.decoded = decoded;
+  next();
+})
+}
+
+
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ftwwmzw.mongodb.net/?retryWrites=true&w=majority`;
@@ -59,6 +77,18 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/users/admin/:email',verifyJWT, async(req,res) => {
+      const email = req.params.email;
+
+      if(req.decoded.email !== email){
+        res.send({admin: false});
+      }
+      const query = {email: email}
+      const user = await usersCollection.findOne(query);
+      const result = {admin: user?.role === 'admin'}
+      res.send(result);
+    })
+
     app.patch(`/users/admin/:id`, async(req,res) => {
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)}
@@ -71,6 +101,18 @@ async function run() {
   
      const result = await usersCollection.updateOne(filter, updateDoc);
      res.send(result);
+    })
+
+    app.get('/users/instructor/:email',verifyJWT, async(req,res) => {
+      const email = req.params.email;
+
+      if(req.decoded.email !== email){
+        res.send({instructor: false});
+      }
+      const query = {email: email}
+      const user = await usersCollection.findOne(query);
+      const result = {instructor: user?.role === 'instructor'}
+      res.send(result);
     })
 
     app.patch(`/users/instructor/:id`, async(req,res) => {
