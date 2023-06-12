01d049b5bb5d79d8a6ed44b077fb6db2144d9e87
unknown <muttakinahmed871@gmail.com>
2023-06-12 18:05:32 +0600

    created make admin api

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ftwwmzw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -49,6 +49,20 @@ async function run() {
       }
       const result =await usersCollection.insertOne(user);
       res.send(result);
+    })
+
+    app.patch(`/users/admin/:id`, async(req,res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = {upsert: true}
+      const updateDoc = {
+        $set: {
+          role:  'admin'
+        },
+      };
+  
+     const result = await usersCollection.updateOne(filter, updateDoc);
+     res.send(result);
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
